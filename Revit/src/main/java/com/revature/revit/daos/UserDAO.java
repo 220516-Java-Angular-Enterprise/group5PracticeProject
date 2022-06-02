7f8aeffa19bc6e474a11578d92dae112eac26198
mb_q <mbquercioli@gmail.com>
2022-06-02 14:11:06 -0600

    userdao setup

--- Revit/src/main/java/com/revature/revit/daos/UserDAO.java
+++ Revit/src/main/java/com/revature/revit/daos/UserDAO.java
@@ -1,13 +1,28 @@
 package com.revature.revit.daos;
 
 import com.revature.revit.models.Users;
-
+import com.revature.revit.util.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 public class UserDAO implements CrudDAO<Users> {
+
+    Connection con = DatabaseConnection.getCon();
     @Override
     public void save(Users obj) {
+        try {
+            PreparedStatement ps = con.prepareStatement("INSERT INTO users (id, username, password VALUES (?,?,?,?)");
+            ps.setString(1, obj.getId());
+            ps.setString(2, obj.getUsername());
+            ps.setString(3, obj.getPassword());
+            ps.executeUpdate();
 
+        } catch (
+                SQLException e) {
+            throw new RuntimeException("An error occurred when saving to the database.");
+        }
     }
 
     @Override
